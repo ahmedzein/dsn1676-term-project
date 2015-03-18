1ba410cec978994557888acedb1a364f829acb51
ahmedzein <elze0010@algonquinlive.com>
2015-03-17 20:50:44 -0400

    js css
    header in the index is functional
    every face in the body has its own class with stat sheet
    js expands the list

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
-var nav = document.querySelector('.panel');
+var $btnSpaceJam = $('.btn-space-jam');
+var $panel = $('.panel');
 
-doucment.querySelector('.btn-space-jam').addEventListener('click', function (e) {
-    e.preventDefault();
-    if(nav.getAttribute('data-state') == 'expanded') {
-        nav.setAttribute('data-state', 'collapsed');
-    this.setAttribute('data-state', 'inactive');
-    }
-    
-    else {
-        nav.setAttribute('data-state', 'expanded');
-        this.setAttribute('data-state','active');
-    }
-});
+$btnSpaceJam.on ('click', function (){
+    $panel.animate ({height: 'toggle'});
+})
+
+
+var $jordan =$('.jordan');
+var $bugs =$('.bugs');
+var $lola =$('.lola');
+var $daffy =$('.daffy');
+var $tas =$('.tas');
+
+var $jordanStat =$('.jordan-stat')
+var $bugsStat =$('.bugs-stat')
+var $lolaStat =$('.lola-stat')
+var $daffyStat =$('.daffy-stat')
+var $tasStat =$('.tas-stat')
+
+$jordan.on('click', function () {
+    $jordanStat.animate({height: 'toggle'});
+})
+
+$bugs.on('click', function () {
+    $bugsStat.animate({height: 'toggle'});
+})
+
+$lola.on('click', function () {
+    $lolaStat.animate({height: 'toggle'});
+})
+
+$daffy.on('click', function () {
+    $daffyStat.animate({height: 'toggle'});
+})
+
+$tas.on('click', function () {
+    $tasStat.animate({height: 'toggle'});
+})
